899f3127338a6b65fca2f8e9062313509257dc12
PFaboyaQA <philip.faboya@QA.com>
2020-03-03 16:56:16 +0000

    http responding

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ app = Flask(__name__)
 def random_number(n=3):
     range_start = 10**(n-1)
     range_end = (10**n)-1
-    return Response(range_start, range_end, mimetype='text/plain')
+    number = str(randint(range_start, range_end))
+    return Response(number, mimetype='text/plain')
 
 
 @app.route('/random_string', methods=['GET'])
 def random_string(size=3, chars=string.ascii_uppercase):
-    string=''.join(random.choice(chars) for _ in range(size))
+    string = ''.join(random.choice(chars) for _ in range(size))
     return Response(string, mimetype='text/plain')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 
